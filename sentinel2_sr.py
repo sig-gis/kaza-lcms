9a585866f4cccbb9286cfc3526e074176a5ac02c
kyle-woodward <kw.geospatial@gmail.com>
2022-06-16 09:51:26 -0400

    exporting monthly composite band stack img

--- sentinel2_sr.py
+++ sentinel2_sr.py
@@ -1,4 +1,6 @@
 import ee
+import covariates
+idx = covariates.indices()
 
 
 ee.Initialize()
@@ -93,48 +95,74 @@ def apply_cld_shdw_mask(img):
     # Subset reflectance bands and update their masks, return the result.
     return img.select('B.*').updateMask(not_cld_shdw)
     
- 
- 
+def add_covariates(img):
+    "Adds select indices to the S2 image, assumes band names have already been renamed (blue,green,red,etc)"
+    covariates = ["blue","green","red","nir","swir1","swir2","EVI","SAVI","IBI",] # full list of unique spectral covariate bands
+    img = ee.Image(img).addBands(idx.EVI(img)).addBands(idx.SAVI(img)).addBands(idx.IBI(img))
+    img = img.select(covariates) # because .addBands() will create duplicates of pre-existing bands
+    return img
+
+def rename_month_bands(img:ee.Image) :
+    def edit_names(str):
+        month_str = ee.String("_").cat(ee.Number.parse(ee.String(str).split('_').get(0)).add(1).format())
+        base = ee.String("_").cat(ee.String(str).split('_').get(1))
+        return ee.String("S2").cat(month_str).cat(base)
+    return ee.Image(img).rename(img.bandNames().map(edit_names))
  
 if __name__ == "__main__":
-	#def composite(aoi,year):
-	
-	ee.Initialize()
-	year = 2021
-	
-	
-	
-	aoi = ee.FeatureCollection("projects/sig-ee/WWF_KAZA_LC/SNMC").geometry().buffer(5000)
+    #def composite(aoi,year):
 
-	
-	CLOUD_FILTER = 70
-	CLD_PRB_THRESH = 40
-	NIR_DRK_THRESH = 0.15
-	CLD_PRJ_DIST = 2
-	BUFFER = 100
+    ee.Initialize()
+    year = 2021
 
-	month = 4+3
-			
-	START_DATE = ee.Date.fromYMD(year,month,1)
-	END_DATE = ee.Date.fromYMD(year,month+3,30)
-			
-			
-	s2_sr_cld_col = get_s2_sr_cld_col(aoi, START_DATE, END_DATE)
 
-			
-	img =  ee.Image(s2_sr_cld_col.map(add_cld_shdw_mask)
-				   .map(apply_cld_shdw_mask).median())
-	
-			
-			
-	img = ee.Image(img).set("system:time_start",ee.Date.fromYMD(year,1,1)).toInt()
-	region =  ee.FeatureCollection("projects/sig-ee/WWF_KAZA_LC/SNMC").geometry().bounds()
-							
-	outputName = "projects/sig-ee/WWF_KAZA_LC/" + str(year)  +str(month).zfill(2) + "_" + str(month+4).zfill(2)
 
-	task_ordered = ee.batch.Export.image.toAsset(image=ee.Image(img).clip(aoi).toInt(), description="Export "+str(year)+"sentinel2_aoi", assetId=outputName,region=region.getInfo()['coordinates'], maxPixels=1e13,scale=10 )
-				
-	task_ordered.start()
+    aoi = ee.FeatureCollection("projects/sig-ee/WWF_KAZA_LC/SNMC").geometry().buffer(5000)
 
- 
+
+    CLOUD_FILTER = 70
+    CLD_PRB_THRESH = 40
+    NIR_DRK_THRESH = 0.15
+    CLD_PRJ_DIST = 2
+    BUFFER = 100
+
+    months = [1,2,3,4,5,6,7,8,9,10,11,12]
+    img_list=[]
+    for month in months:
+        
+        START_DATE = ee.Date.fromYMD(year,month,1)
+        END_DATE = ee.Date.fromYMD(year,month,30)
+        
+        if month == 2:
+            END_DATE = ee.Date.fromYMD(year,month,28)
+             
+        s2_sr_cld_col = get_s2_sr_cld_col(aoi, START_DATE, END_DATE)
+
+                
+        img =  ee.Image(s2_sr_cld_col.map(add_cld_shdw_mask)
+                    .map(apply_cld_shdw_mask).median()
+                    .toInt().select(["B2","B3","B4","B8","B11","B12"],['blue','green','red','nir','swir1','swir2'])
+                    )
+        #print(img.bandNames().size().getInfo())
+        img = add_covariates(img)
+        #print(month,img.bandNames().size().getInfo())                
+        
+        img = ee.Image(img).set("system:time_start",ee.Date.fromYMD(year,month,1))
+        img_list.append(img)
+        
+      
+    stack = ee.ImageCollection.fromImages(img_list).toBands()
+    #print(stack.bandNames().getInfo())
+    
+    topo = idx.addTopography(img).select("elevation","slope","aspect","eastness","northness")
+    stack = rename_month_bands(stack).addBands(topo)
+    #print(stack.bandNames().getInfo())
   
+    region =  ee.FeatureCollection("projects/sig-ee/WWF_KAZA_LC/SNMC").geometry().bounds()
+                                
+    outputName = "projects/sig-ee/WWF_KAZA_LC/" + "S2_" + str(year) + "monthlyCompositeStack"#.zfill(2) #+ "_" + str(month+4).zfill(2)
+
+    task_ordered = ee.batch.Export.image.toAsset(image=ee.Image(img).clip(aoi).toInt(), description="Export "+str(year)+"monthlyCompositeStack"+"sentinel2_aoi", assetId=outputName,region=region.getInfo()['coordinates'], maxPixels=1e13,scale=10 )
+                    
+    task_ordered.start()
+    print(f"export started: {outputName}")
